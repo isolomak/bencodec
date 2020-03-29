674d21e415e3723275eec62956bd5f71aa015392
IvanSolomakhin <solomakhinv@gmail.com>
2020-03-29 14:23:02 +0300

    enforce testing

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,13 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     verbose: true,
+    collectCoverage: true,
+    coverageThreshold: {
+        global: {
+            branches: 90,
+            functions: 90,
+            lines: 90,
+            statements: 90,
+        }
+    }
 };
