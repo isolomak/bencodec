688dc8e7d5e0b6380bd12f9e47909e159ee6faec
IvanSolomakhin <solomakhinv@gmail.com>
2020-03-29 15:26:11 +0300

    enforce testing

--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,10 @@ module.exports = {
     collectCoverage: true,
     coverageThreshold: {
         global: {
-            branches: 90,
-            functions: 90,
-            lines: 90,
-            statements: 90,
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
         }
     }
 };
